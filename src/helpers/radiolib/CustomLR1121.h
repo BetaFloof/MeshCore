08a851dc86248ad0a33b015e5a340f53a012029e
BetaFloof <betafloof@gmail.com>
2026-04-19 11:42:16 +0200

    adjust get/set RxBoostedGainMode Align with recent changes

--- src/helpers/radiolib/CustomLR1121.h
+++ src/helpers/radiolib/CustomLR1121.h
@@ -4,10 +4,18 @@
 #include "MeshCore.h"
 
 class CustomLR1121 : public LR1121 {
+  bool _rx_boosted = false;
   public:
     CustomLR1121(Module *mod) : LR1121(mod) { }
 
     float getFreqMHz() const { return freqMHz; }
+
+    int16_t setRxBoostedGainMode(bool en) {
+      _rx_boosted = en;
+      return LR1121::setRxBoostedGainMode(en);
+    }
+
+    bool getRxBoostedGainMode() const { return _rx_boosted; }
 
     bool isReceiving() {
       uint16_t irq = getIrqStatus();
