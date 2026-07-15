1bcd6ba23a10372f2f891d5b383913e86980cdd7
BetaFloof <betafloof@gmail.com>
2026-07-16 00:05:35 +0200

    adjust for recent changes

--- src/helpers/radiolib/CustomLR1121Wrapper.h
+++ src/helpers/radiolib/CustomLR1121Wrapper.h
@@ -7,6 +7,15 @@
 class CustomLR1121Wrapper : public RadioLibWrapper {
 public:
   CustomLR1121Wrapper(CustomLR1121& radio, mesh::MainBoard& board) : RadioLibWrapper(radio, board) { }
+
+  void setParams(float freq, float bw, uint8_t sf, uint8_t cr) override {
+    ((CustomLR1121 *)_radio)->setFrequency(freq);
+    ((CustomLR1121 *)_radio)->setSpreadingFactor(sf);
+    ((CustomLR1121 *)_radio)->setBandwidth(bw);
+    ((CustomLR1121 *)_radio)->setCodingRate(cr);
+    updatePreamble(sf);
+  }
+
   void doResetAGC() override { lr11x0ResetAGC((LR11x0 *)_radio, ((CustomLR1121 *)_radio)->getFreqMHz()); }
   bool isReceivingPacket() override { 
     return ((CustomLR1121 *)_radio)->isReceiving();
@@ -17,16 +26,23 @@ public:
     return rssi;
   }
 
+  uint32_t getEstAirtimeFor(int len_bytes) override {
+    auto airtime = RadioLibWrapper::getEstAirtimeFor(len_bytes);
+    return airtime < 200 ? 200 : airtime;   // at least 200 millis
+  }
+
   void onSendFinished() override {
     RadioLibWrapper::onSendFinished();
-    _radio->setPreambleLength(16); // overcomes weird issues with small and big pkts
+    _radio->setPreambleLength(preambleLengthForSF(getSpreadingFactor())); // overcomes weird issues with small and big pkts
   }
 
   float getLastRSSI() const override { return ((CustomLR1121 *)_radio)->getRSSI(); }
   float getLastSNR() const override { return ((CustomLR1121 *)_radio)->getSNR(); }
   
-  void setRxBoostedGainMode(bool en) override {
-    ((CustomLR1121 *)_radio)->setRxBoostedGainMode(en);
+  uint8_t getSpreadingFactor() const override { return ((CustomLR1121 *)_radio)->getSpreadingFactor(); }
+
+  bool setRxBoostedGainMode(bool en) override {
+    return ((CustomLR1121 *)_radio)->setRxBoostedGainMode(en) == RADIOLIB_ERR_NONE;
   }
   bool getRxBoostedGainMode() const override {
     return ((CustomLR1121 *)_radio)->getRxBoostedGainMode();
